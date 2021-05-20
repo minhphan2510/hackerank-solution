77f4fca8bf6b274a93fe29f5d9a9fec30f9b43f0
Minh Phan <minh.phan@enouvo.com>
2021-05-20 23:03:54 +0700

    no.1 solve me first

---
+++ algorithms/solvemefirst/index.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function (inputStdin: string): void {
+	inputString += inputStdin;
+});
+
+process.stdin.on('end', function (): void {
+	inputLines = inputString.split('\n');
+	inputString = '';
+	main();
+});
+
+function readLine(): string {
+	return inputLines[currentLine++];
+}
+
+function main() {
+	// Enter your code here
+	const result = parseInt(inputLines[0]) + parseInt(inputLines[1]);
+	console.log(result);
+}
